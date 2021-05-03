2ebd73d86c577b98958acfdfdfa16b7a76ae2bb8
Ross MacArthur <ross@macarthur.io>
2021-05-03 22:51:48 +0200

    Add default "search" item at end of every query
    This means if we can't find a package locally then there will still be
    a script filter item that will open the respective search endpoint for
    the site.

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,60 @@ use std::env;
 use std::iter;
 
 use anyhow::Result;
+use powerpack::{Icon, Item, ModifierData, ModifierKey};
 
-fn to_item(pkg: registry::Package) -> powerpack::Item<'static> {
-    powerpack::Item::new(format!("{} v{}", pkg.name, pkg.vers))
+fn default(query: &str) -> Item<'static> {
+    Item::new(format!("Search for '{}'", query))
+        .subtitle(format!("Search Crates.io for '{}' →", query))
+        .arg(format!("https://crates.io/search?q={}", query))
+        .icon(Icon::new("icon.png"))
+        .modifier(
+            ModifierKey::Option,
+            ModifierData::new()
+                .subtitle(format!("Search Lib.rs for '{}' →", query))
+                .arg(format!("https://lib.rs/search?q={}", query)),
+        )
+        .modifier(
+            ModifierKey::Shift,
+            ModifierData::new()
+                .subtitle(format!("Search Docs.rs for '{}' →", query))
+                .arg(format!("https://docs.rs/releases/search?query={}", query)),
+        )
+}
+
+fn to_item(pkg: registry::Package) -> Item<'static> {
+    Item::new(format!("{} v{}", pkg.name, pkg.vers))
         .subtitle("Open in Crates.io →")
         .arg(format!("https://crates.io/crates/{}", pkg.name))
-        .icon(powerpack::Icon::new("icon.png"))
+        .icon(Icon::new("icon.png"))
         .modifier(
-            powerpack::ModifierKey::Option,
-            powerpack::ModifierData::new()
+            ModifierKey::Option,
+            ModifierData::new()
                 .subtitle("Open in Lib.rs →")
                 .arg(format!("https://lib.rs/crates/{}", pkg.name)),
         )
         .modifier(
-            powerpack::ModifierKey::Shift,
-            powerpack::ModifierData::new()
+            ModifierKey::Shift,
+            ModifierData::new()
                 .subtitle("Open in Docs.rs →")
                 .arg(format!("https://docs.rs/{}", pkg.name)),
         )
 }
 
-fn output(pkgs: impl Iterator<Item = registry::Package>) -> Result<()> {
-    Ok(powerpack::output(pkgs.map(to_item))?)
+fn output(query: &str) -> Result<()> {
+    powerpack::output(
+        registry::walk(query)?
+            .take(50)
+            .map(to_item)
+            .chain(iter::once(default(query))),
+    )?;
+    Ok(())
 }
 
 fn main() -> Result<()> {
-    match env::args().nth(1).map(|q| q.trim().to_string()) {
-        None => output(iter::empty()),
-        Some(query) => output(registry::walk(&query)?.take(50)),
+    match env::args().nth(1).as_deref().map(str::trim) {
+        None | Some("") => powerpack::output(iter::empty())?,
+        Some(query) => output(query)?,
     }
+    Ok(())
 }
